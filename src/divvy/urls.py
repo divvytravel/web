1e1309b2fc9198b628e64063d5ce1649be3eada3
Igor Petrov <igor.s.petrov@gmail.com>
2014-08-17 23:47:53 +0400

    Create template for test index page

--- src/divvy/urls.py
+++ src/divvy/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,4 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index-page'),
 )
