8e51ae301323036447bd85f6e96d4fdf7d5e0c20
Igor Petrov <igor.s.petrov@gmail.com>
2014-08-17 19:48:15 +0400

    Define language settings

--- src/divvy/settings/common.py
+++ src/divvy/settings/common.py
@@ -74,7 +74,12 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
+
+LANGUAGES = (
+    ('en', 'EN'),
+    ('ru', 'RU'),
+)
 
 TIME_ZONE = 'UTC'
 
