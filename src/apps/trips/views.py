879908a726c777f4717ea12e9eb3dd8d9c0cdded
Igor Petrov <igor.s.petrov@gmail.com>
2014-08-29 00:37:56 +0400

    Create simple trip detail view

--- src/apps/trips/views.py
+++ src/apps/trips/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+
+from .models import Trip
+
+def trip_detail(request, pk=None):
+    trip = get_object_or_404(Trip, pk=pk)
+    return render(request, 'detail.html', dict(trip=trip,))
