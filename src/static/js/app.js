f0db667e689bccae3294e3ac1d47c4e102343d4e
Igor Petrov <igor.s.petrov@gmail.com>
2014-08-30 20:37:34 +0400

    Create test form. Modify knockout app and trip list template.

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,19 +2,30 @@ function Trip(data) {
     this.title = data.title;
     this.start_date = data.start_date;
     this.end_date = data.end_date;
+    this.people_min_count = data.people_min_count;
+    this.main_photo = data.main_photo;
+    this.photos = data.photos;
 }
 
 function TripListViewModel() {
     var self = this;
 
+    self.min_people = ko.observable('');
+    self.max_people = ko.observable('');
+
     self.trips = ko.observableArray([]);
 
-    self.updateTrips = function() {
-        $.getJSON("/api/trips/", function(allData) {
+    self.get_params = ko.computed(function() {
+        return "?min_people=" + self.min_people() +
+               "&max_people=" + self.max_people();
+    }, this);
+
+    ko.computed(function() {
+        $.getJSON("/api/trips/" + self.get_params(), function(allData) {
             var mappedTasks = $.map(allData, function(item) { return new Trip(item) });
             self.trips(mappedTasks);
         });
-    };
+    }).extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
 }
 
 ko.applyBindings(new TripListViewModel());
