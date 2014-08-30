d31eddb9293b74657726197b36b22035cd72e5da
Igor Petrov <igor.s.petrov@gmail.com>
2014-08-30 13:28:43 +0400

    Add trip request to trip detail view

--- src/apps/trips/views.py
+++ src/apps/trips/views.py
@@ -2,8 +2,17 @@
 
 from django.shortcuts import render, get_object_or_404
 
-from .models import Trip
+from .models import Trip, TripRequest
 
 def trip_detail(request, pk=None):
     trip = get_object_or_404(Trip, pk=pk)
-    return render(request, 'detail.html', dict(trip=trip,))
+
+    try:
+        if request.user:
+            triprequest = TripRequest.objects.get(trip=trip, user=request.user)
+        else:
+            triprequest = None
+    except TripRequest.DoesNotExist:
+        triprequest = None
+
+    return render(request, 'detail.html', dict(trip=trip, triprequest=triprequest, ))
