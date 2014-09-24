1f1c2edac9e012e4705408f4ee961cbc4aca5390
Igor Petrov <igor.s.petrov@gmail.com>
2014-09-24 15:02:53 +0400

    Add function to get user avatar URL

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -7,3 +7,7 @@ from django.utils.translation import ugettext as _
 
 class DivvyUser(AbstractUser):
     birthday = models.DateField(_(u'Birthday'), blank=True, null=True)
+
+    def get_avatar_url(self):
+        return u'http://graph.facebook.com/%s/picture?type=large&width=90&height=90' % \
+               self.social_auth.get().uid
